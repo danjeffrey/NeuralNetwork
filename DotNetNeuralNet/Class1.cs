14f0da5d4b023a45d4dd7688a3bc64f995b9df9d
danjeffrey <dan@apexvis.com>
2015-09-24 11:29:19 -0700

    create class library project for .NET neural netowkr solver.

---
+++ DotNetNeuralNet/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+  Provide a simple API for defining a neural network, 
+  feeding test data, telling it to learn its parameters,
+  and then using it to generate proedictions for new data 
+  sets. 
+
+  This is a learning exercise and not intended to be 
+  a state of the art or commercial grade tool.  
+ **/
+namespace DotNetNeuralNet
+{
+    public class Class1
+    {
+    }
+}
